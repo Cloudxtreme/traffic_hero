201e5fad6b219edb5ad713089ea296580a80ca2f
sabsowitz <sabsowitz@yahoo.com>
2014-12-16 09:51:45 -0500

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,35 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to Traffic Hero!"
+      redirect_back_or @user
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Before filters
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  resources :users
   root 'static_pages#home'
   match '/help',      to: 'static_pages#help',      via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
