56afe0e6322e46287fbaf18653b53200decd71b2
sabsowitz <sabsowitz@yahoo.com>
2014-12-16 10:23:58 -0500

    Sign in / Sign out finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'static_pages#home'
   match '/help',      to: 'static_pages#help',      via: 'get'
-  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signup',    to: 'users#new',              via: 'get'
 end
