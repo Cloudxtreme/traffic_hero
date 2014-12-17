031799ba060da5b2a260e01a0b9679c519aa5c4d
sabsowitz <sabsowitz@yahoo.com>
2014-12-17 10:52:56 -0500

    Google maps implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  
+
   root 'users#new'
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-
+  match '/signup',    to: 'users#new',              via: 'get'
+  match '/signin',    to: 'sessions#new',           via: 'get'
   delete '/logout', to: 'sessions#destroy', as: 'signout'
 
   match '/help',      to: 'static_pages#help',      via: 'get'
   match '/about',     to: 'static_pages#about',     via: 'get'
   match '/contact',   to: 'static_pages#contact',   via: 'get'
-  match '/signup',    to: 'users#new',              via: 'get'
-  match '/signin',    to: 'sessions#new',           via: 'get'
+
+  get 'maps/index'
+  get 'maps/show'
+
   
 end
